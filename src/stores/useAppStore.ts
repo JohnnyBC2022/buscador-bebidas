fd92b0fc0d10f75ef2df2e724ce6fe562711bd10
Johnny <jonathanbc2012@gmail.com>
2025-01-15 20:47:12 +0100

    feat: notificationSlice created and added to useAppStore

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,11 +1,13 @@
 import {create} from 'zustand'
-import { createRecipesSlice, RecipesSliceType } from './recipeSlice'
 import { devtools } from 'zustand/middleware'
+import { createRecipesSlice, RecipesSliceType } from './recipeSlice'
 import {createFavoritesSlice,FavoritesSliceType} from './favoritesSlice'
+import {createNotificationSlice,NotificationSliceType} from './notificationSlice'
 
-export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools ( (...a)=> ({
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType & NotificationSliceType>()(devtools ( (...a)=> ({
     ...createRecipesSlice(...a),
     ...createFavoritesSlice(...a),
+    ...createNotificationSlice(...a),
 })))
 
 // Slice Pattern
