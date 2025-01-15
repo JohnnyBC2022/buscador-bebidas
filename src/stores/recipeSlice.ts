e456efab5b8edc626c827441b5f825a44aeaf5ea
Johnny <jonathanbc2012@gmail.com>
2025-01-15 12:26:32 +0100

    feat: getRecipes added to recipeSlice

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,24 +1,31 @@
 import { StateCreator } from "zustand"
 import { getCategories, getRecipes } from "../services/RecipeService"
-import type { Categories, SearchFilter } from "../types"
+import type { Categories, Drinks, SearchFilter } from "../types"
 
 export type RecipesSliceType = {
     categories: Categories
+    drinks: Drinks
     fetchCategories: () => Promise<void>
-    searchRecipes: (searchFilter : SearchFilter) => Promise<void>
+    searchRecipes: (searchFilter: SearchFilter) => Promise<void>
 }
 
-export const createRecipesSlice : StateCreator<RecipesSliceType>= (set)=>({
+export const createRecipesSlice: StateCreator<RecipesSliceType> = (set) => ({
     categories: {
         drinks: []
     },
-    fetchCategories: async () =>{
+    drinks: {
+        drinks: []
+    },
+    fetchCategories: async () => {
         const categories = await getCategories()
         set({
             categories
         })
     },
-    searchRecipes: async (filters)=>{
-        await getRecipes(filters)
+    searchRecipes: async (filters) => {
+        const drinks = await getRecipes(filters)
+        set({
+            drinks
+        })
     }
 })
