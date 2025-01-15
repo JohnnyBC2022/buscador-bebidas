8a3816e24243a04b72f762294e713221d34bf453
Johnny <jonathanbc2012@gmail.com>
2025-01-15 20:34:11 +0100

    feat: favorites added to favoritePage

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,7 +1,16 @@
-
+import DrinkCard from "../components/DrinkCard";
+import { useAppStore } from "../stores/useAppStore";
 
 export default function FavoritesPage() {
+  const favorites = useAppStore((state) => state.favorites);
   return (
-    <h1>Favoritos</h1>
-  )
+    <>
+      <h1 className="text-6xl font-extrabold">Favoritos</h1>
+      <section className="grid grid-cols-1 gap-10 my-10 md:grid-cols-2 2xl:grid-cols-4">
+        {favorites.map((recipe) => (
+          <DrinkCard key={recipe.idDrink} drink={recipe} />
+        ))}
+      </section>
+    </>
+  );
 }
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -2,25 +2,23 @@ import { useMemo } from "react";
 import { useAppStore } from "../stores/useAppStore";
 import DrinkCard from "../components/DrinkCard";
 
-
 export default function IndexPage() {
-  const drinks = useAppStore((state)=>state.drinks)
+  const drinks = useAppStore((state) => state.drinks);
 
-  const hasDrinks = useMemo(()=>drinks.drinks.length,[drinks])
+  const hasDrinks = useMemo(() => drinks.drinks.length, [drinks]);
   return (
     <>
       <h1 className="text-6xl font-extrabold">Recetas</h1>
       {hasDrinks ? (
         <section className="grid grid-cols-1 gap-10 my-10 md:grid-cols-2 2xl:grid-cols-4">
-        {drinks.drinks.map((drink)=> (
-          <DrinkCard 
-            key={drink.idDrink}
-            drink={drink}
-          />
-        ) )}
+          {drinks.drinks.map((drink) => (
+            <DrinkCard key={drink.idDrink} drink={drink} />
+          ))}
         </section>
-      ): (
-        <p className="my-10 text-2xl text-center">Utiliza el formulario para encontrar recetas.</p>
+      ) : (
+        <p className="my-10 text-2xl text-center">
+          Utiliza el formulario para encontrar recetas.
+        </p>
       )}
     </>
   );
