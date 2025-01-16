bdf1c5a6d9bc45caa0595019638ea31582a9c904
Johnny <jonathanbc2012@gmail.com>
2025-01-16 12:47:36 +0100

    feat: performance improved

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,34 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IndexPage from "./pages/IndexPage";
-import FavoritesPage from "./pages/FavoritesPage";
 import Layout from "./layouts/Layout";
+
+// Al hacer esta importación de esta manera, lo que conseguimos es que no se descargan todas las páginas, al hacer el build de la aplicación no lo construye todo en un solo archivo, el de la página de favoritos lo construye en otro archivo y solo se descargará esta página cuando se acceda a ella, lo que mejora la performance de la página web y la experiencia de la página será mejor
+const IndexPage = lazy(() => import("./pages/IndexPage"));
+const FavoritesPage = lazy(() => import("./pages/FavoritesPage"));
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<IndexPage />} index/>
-          <Route path="/favoritos" element={<FavoritesPage />} />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback="Cargando...">
+                <IndexPage />
+              </Suspense>
+            }
+            index
+          />
+          <Route
+            path="/favoritos"
+            element={
+              // Aquí se puede sustituir por un spinner de carga
+              <Suspense fallback="Cargando...">
+                <FavoritesPage />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
