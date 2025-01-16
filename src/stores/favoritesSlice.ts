be6deb83672ab73137e5c60c37089c87dc24f88a
Johnny <jonathanbc2012@gmail.com>
2025-01-16 12:12:27 +0100

    feat: showNotification created

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -1,6 +1,7 @@
 import { StateCreator } from "zustand";
 import { Recipe } from "../types";
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
+import { createNotificationSlice, NotificationSliceType } from "./notificationSlice";
 
 export type FavoritesSliceType = {
     favorites: Recipe[]
@@ -10,17 +11,25 @@ export type FavoritesSliceType = {
 }
 
 // Esta sintaxis tan larga es para tener slice anidados, ya que aquí queremos acceder a closeModal que está en el otro slice, así que se deben comunicar en ambos sentidos. Para el cierre del modal hubiera sido más sencillo hacerlo desde RecipeModal, pero así vemos como se comunican y pasan datos de un slice a otro
-export const createFavoritesSlice: StateCreator<FavoritesSliceType & RecipesSliceType, [], [], FavoritesSliceType> = (set, get, api) => ({
+export const createFavoritesSlice: StateCreator<FavoritesSliceType & RecipesSliceType & NotificationSliceType, [], [], FavoritesSliceType> = (set, get, api) => ({
     favorites: [],
     handleClickFavorite: (recipe) => {
         if (get().favoriteExists(recipe.idDrink)) {
             set((state) => ({
                 favorites: state.favorites.filter(favorite => favorite.idDrink !== recipe.idDrink)
             }))
+            createNotificationSlice(set, get, api).showNotification({
+                text: 'Receta eliminada de favoritos',
+                error: false
+            })
         } else {
             set((state) => ({
                 favorites: [...state.favorites, recipe]
             }))
+            createNotificationSlice(set, get, api).showNotification({
+                text: 'Receta añadida a favoritos',
+                error: false
+            })
         }
         createRecipesSlice(set, get, api).closeModal()
         localStorage.setItem('favorites', JSON.stringify(get().favorites))
